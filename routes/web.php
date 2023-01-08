6c73a9f6cfacfad6a6049c1fc4335931a5c24627
KAMRUL ISLAM <kamrulislamhridoy1.0@gmail.com>
2023-01-08 12:18:30 +0600

    my first commit: admin views auth created

---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//what need to to create project app syncronously
+  //1. laravel installtion
+  //2. Database Configuration
+  //3. Setting up views
+  //4. User table Migration
+  //5. Relation setup and data entry.
+  //6. Testing relation with tinker.
+  //7. Admin controller and routes.
+  //8. Testing Methods.
+  //9. Installing node.js and file downloads.
+  //10. Gulp and assets.
+  //11. Admin master file and downloads file.
+  //12. Version Controll and modifing master.
+  //13. Displaing users.
+  //14. Creating page.
+  //15. Laravel Collective html and package
+  //16. Testing form and creating form fields
+
+//Bash control: imported dependecies, composer.
+ // 1. composer create-project --prefer-dist laravel/laravel codewithK
+ // 2. php artisan migrate
+ // 3. composer require laravel/ui
+ // 4. php artisan ui vue --auth
+
+
+
+
+ //git control:
+ //1. git init
+ //2. git status
+ //3. git add .
+ //3 
